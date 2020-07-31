29f7f26707be4bc67c114c4d71891b5dcf0bcc74
Tomer Keizler <tomerke@mta.ac.il>
2020-07-31 04:16:11 +0300

    made a correction in basic grid of players

--- server/mongo.py
+++ server/mongo.py
@@ -7,11 +7,11 @@ def get_final_team():
     client = MongoClient("mongodb+srv://Wolfson:Noy123123@fantasy-9amla.mongodb.net/test?retryWrites=true&w=majority")  ## USER AND PASS
     db = client["Fantasy"]
     collection = db["Players_data"]
-    single_player = collection.find({ "price": "14" })
+    single_player = collection.find_one()
 
     team_players = []
     for i in range(0,11):
-        team_players.append(single_player[0])
+        team_players.append(single_player)
     team_players = json_util.dumps(team_players)
 
     return team_players
