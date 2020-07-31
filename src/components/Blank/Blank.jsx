b79c65f45db72bf769f1e5fcb186e667ea1eb98e
noywolfson <noy@wolfson.co.il>
2020-07-31 13:09:42 +0300

    added welcome message

--- src/components/Blank/Blank.jsx
+++ src/components/Blank/Blank.jsx
@@ -4,7 +4,7 @@ const Blank = () => {
   return <main id="mainContent">
     <div className="container">
       <div className="row justify-content-center mt-5 p-0">
-        <h3>Blank</h3>
+        <h3>Welcome to your fantasy league!</h3>
       </div>
     </div>
   </main>;
