ce64c7d454832204f308c4adc3082e6ea94acf4d
Tomer Keizler <tomerke@mta.ac.il>
2020-09-13 04:47:54 +0300

    added wide information in About us page

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,12 +1,59 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Chip from '@material-ui/core/Chip';
+
+const useStyles = makeStyles((theme) => ({
+  chip: {
+    fontSize: '1.5rem',
+    padding: 18,
+    marginBottom: 20,
+  }
+}));
+
 
 const AboutUs = () => {
-  return <main id="mainContent">
+  const classes = useStyles();
+
+  return (
     <div className="container">
-      <div className="row justify-content-center mt-5 p-0">
-        <h3>Welcome to your fantasy league!</h3>
-      </div>
+
+      <center>
+        <Chip className={classes.chip} color="primary" label="Welcome to Fantaze!" />
+      </center>
+
+      <Typography variant="h6">
+        The Fantasy sports industry is growing tremendously. As of 2020, Fantasy sports is a $8 billion industry with 60 million fantasy players worldwide.
+        In this type of game, participants assemble imaginary teams of real players of a professional sport. These teams compete based on the statistical performance of those players in actual games.
+      </Typography>
+      <br />
+
+      <Typography variant="h6">
+        <b>
+          In Fantaze, we expertise in the area of football. We support UEFA Champions League Fantasy players worldwide, allowing them to maximize their Fantasy performances based on our excellent football data analyzing skills.
+        </b>
+      </Typography>
+      <br />
+
+      <Typography variant="h6">
+        We take into account a lot of football statistics, such as goals scored, assits given, shots taken, fouls commited and more. We combine these parameters and provide an Ultimate team of players, in order to give you an absolute advantage on your Fantazy opponent players and raising your chance to win top prizes.
+      </Typography>
+      <br />
+
+      <Typography variant="h6">
+        Additionally, we take your football preferences into account, allowing you to pick your desired football formation and choose your favorite players.
+      </Typography>
+      <br />
+
+      <Typography variant="body1">
+        The football preformances data was collected from API-Football with rapidAPI subscription, and stored in our Atlas mongoDB.
+          <br />
+        This app was developed with Python, javascript and React js.
+          <br />
+        The Client-Server communication is based on Flask.
+      </Typography>
+
     </div>
-  </main>;
+  )
 }
 export default AboutUs;
