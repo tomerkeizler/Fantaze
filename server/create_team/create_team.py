24f1d0938fbe444befa6658b5df7943c35bd4da8
noywolfson <noy@wolfson.co.il>
2020-09-15 02:43:56 +0300

    change player struct, adding performance details to player

--- server/create_team/create_team.py
+++ server/create_team/create_team.py
@@ -13,21 +13,54 @@ MIN_PRICE = 3
 
 def convert_performances_list_to_avg(id_playerData_map):
     for player in id_playerData_map.values(): #.items(): 
-        avg = int(math.floor(sum(player['performance']) / len(player["performance"])))
-        player["performance"] = avg
+        avg_performance = int(math.floor(sum(player['performance']) / len(player["performance"])))
+        player["performance"] = avg_performance
+        player['minutes_played'] = sum(player['minutes_played']) / len (player['minutes_played'])
+        player['passes_accuracy'] = sum(player['passes_accuracy']) / len(player['passes_accuracy'])
     return id_playerData_map
 
-def update_id_playersData_map(id_playersData_map, current_player, performances):
+def update_fields(id_playersData_map, performances_details):
+    player_id = performances_details["player_id"]
+    id_playersData_map[player_id]['performance'].append(performances_details['performance'])
+    id_playersData_map[player_id]['startXI'] += performances_details['startXI']
+    id_playersData_map[player_id]['substitute'] += performances_details['startXI'] == False and performances_details['minutes_played'] > 0 
+    id_playersData_map[player_id]['goals'] += performances_details['goals']['total']
+    id_playersData_map[player_id]['assists'] += performances_details['goals']['assists'] 
+    id_playersData_map[player_id]['yellow'] += performances_details['cards']['yellow'] 
+    id_playersData_map[player_id]['red'] += performances_details['cards']['red'] 
+    id_playersData_map[player_id]['minutes_played'].append(performances_details['minutes_played']) 
+    id_playersData_map[player_id]['passes_accuracy'].append(performances_details['passes']['accuracy'])
+    if id_playersData_map[player_id].get('conceded') is not None:
+        id_playersData_map[player_id]['conceded'] += performances_details['goals']['conceded']
+    return id_playersData_map
+
+def get_player_struct(current_player):
+    player_struct = {
+        "player_id" : current_player["player_id"],
+        "player_name" : current_player["player_name"],
+        "price" : current_player["price"],
+        "performance" : [],
+        "position" : current_player["position"],
+        "team_id" : current_player["team_id"],
+        "team_name" : current_player["team_name"],
+        'startXI' : 0,
+        'substitute': 0,
+        'goals': 0, 
+        'assists': 0,  
+        'yellow': 0,
+        'red': 0,
+        'minutes_played': [],
+        'passes_accuracy': []
+    }
+    if current_player['position'] == "Goalkeeper" or current_player['position'] == "Defender":
+        player_struct['conceded'] = 0
+    return player_struct
+
+def update_id_playersData_map(id_playersData_map, current_player, performances_details):
     player_id = current_player["player_id"]
-    id_playersData_map[player_id] = {
-                                    "player_id" : player_id,
-                                    "player_name" : current_player["player_name"],
-                                    "price" : current_player["price"],
-                                    "performance" : performances,
-                                    "position" : current_player["position"],
-                                    "team_id" : current_player["team_id"],
-                                    "team_name" : current_player["team_name"]
-                                    }
+    if player_id not in id_playersData_map:
+        id_playersData_map[player_id] = get_player_struct(current_player)
+    update_fields(id_playersData_map, performances_details)
     return id_playersData_map
 
 def create_id_players_map():
@@ -90,12 +123,7 @@ def create_id_playerData_map(season, round):
         if all_performances[i]["event_id"] in fixtures and all_performances[i]["player_id"] in id_players_map:
             current_player = id_players_map[all_performances[i]["player_id"]]
             if current_player["price"] != 0:
-                player_id = current_player["player_id"]
-                performances = []
-                if player_id in id_playersData_map:
-                    performances = (id_playersData_map.get(player_id))["performance"]
-                performances.append(all_performances[i]["performance"])
-                update_id_playersData_map(id_playersData_map, current_player, performances)
+                update_id_playersData_map(id_playersData_map, current_player, all_performances[i])
     return id_playersData_map
 
 def create_id_playerDataAvg_map(season, round):
@@ -142,8 +170,10 @@ def get_chosen_players(playersId_players_map, chosen_players_id_list):
         if not is_eliminated(id):
             player = playersId_players_map.get(id)
             if player is None: 
-                playersId_players_map = update_id_playersData_map(playersId_players_map, id_players_map.get(id), 0)
-                player = playersId_players_map.get(id)
+                player_struct = get_player_struct(id_players_map[id])
+                player_struct['performance'] = 0
+                # playersId_players_map = update_id_playersData_map(playersId_players_map, id_players_map.get(id), 0)
+                player = player_struct
             chosen_players.append(player)
     return chosen_players
 
@@ -170,8 +200,11 @@ def get_eliminated_players_from_constraints():
         relevant_teams = qualified_teams_id_by_year_round[season][round]
         for id in chosen_players_id_list:
             if(id_players_map[id]["team_id"]) not in relevant_teams:
-                update_id_playersData_map(id_players_map, id_players_map[id], 0)
-                eliminated_players.append(id_players_map[id])
+                player_struct = get_player_struct(id_players_map[id])
+                player_struct['performance'] = 0
+                eliminated_players.append(player_struct)
+                # update_id_playersData_map(id_players_map, id_players_map[id], 0)
+                # eliminated_players.append(id_players_map[id])
     fantasy_team_data['eliminated_players'] = eliminated_players
     return eliminated_players
 
@@ -317,52 +350,8 @@ def get_team():
     team['players'].extend(chosen_players)
     return team['players']
 
-def update_performances(player_performance, performance):
-    player_performance['startXI'] += performance['startXI']
-    player_performance['substitute'] += performance['startXI'] == False and performance['minutes_played'] > 0
-    player_performance['goals'] += performance['goals']['total']
-    player_performance['assists'] += performance['goals']['assists']
-    player_performance['yellow'] += performance['cards']['yellow']
-    player_performance['red'] += performance['cards']['red']
-    player_performance['minutes_played'].append(performance['minutes_played'])
-    player_performance['passes_accuracy'].append(performance['passes']['accuracy'])
-    if player_performance.get('conceded') is not None:
-        player_performance['conceded'] += performance['goals']['conceded']
-    
-    return player_performance        
-
-def get_player_performances_struct():
-    player_performances = {
-        'startXI' : 0,
-        'substitute': 0,
-        'goals': 0, 
-        'assists': 0,  
-        'yellow': 0,
-        'red': 0,
-        'minutes_played': [],
-        'passes_accuracy': []
-    }
-    return player_performances
-
-def get_player_performances(player_id):
-    season = fantasy_team_data['season']
-    round = fantasy_team_data['round']
-    player_performance = get_player_performances_struct()
-    fixtures = get_fixtures(season, round)
-    
-    for i in range(len(all_performances)):
-        if all_performances[i]["event_id"] in fixtures and player_id == all_performances[i]["player_id"]:
-            if all_performances[i]['position'] == "G" or all_performances[i]['position'] == "D":
-                if not player_performance.get('conceded'): 
-                    player_performance['conceded'] = 0
-            player_performance = update_performances(player_performance, all_performances[i])
-            
-    player_performance['minutes_played'] = sum(player_performance['minutes_played']) / len (player_performance['minutes_played'])
-    player_performance['passes_accuracy'] = sum(player_performance['passes_accuracy']) / len(player_performance['passes_accuracy'])
-    return player_performance
 
 all_fixtures_data = mongo.find_from_collection(mongo.data_fixtures_collection, {})
 all_performances = mongo.find_from_collection(mongo.player_performances_collection, {})
 id_players_map = create_id_players_map()
-# get_player_performances(505)
 # get_team()
