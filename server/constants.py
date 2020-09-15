6b60e5a51620b1614bfff857ea65b6fc25b26dd7
noywolfson <noy@wolfson.co.il>
2020-09-15 16:10:28 +0300

    update port in constants file

--- server/constants.py
+++ server/constants.py
@@ -1,7 +1,7 @@
 import os
 
 CONSTANTS = {
-    'PORT': os.environ.get('PORT', 'https://my-fantaze-app.herokuapp.com'),
+    'PORT': os.environ.get('PORT', 'https://my-fantaze-app.herokuapp.com/api/my_team/get_ultimate_team'),
     'HTTP_STATUS': {
         '404_NOT_FOUND': 404,
         '200_OK': 200,
