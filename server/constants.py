1a94594220720422794b8b5409945e5ee57bff97
noywolfson <noy@wolfson.co.il>
2020-09-15 16:02:39 +0300

    change port on constants file

--- server/constants.py
+++ server/constants.py
@@ -1,7 +1,7 @@
 import os
 
 CONSTANTS = {
-    'PORT': os.environ.get('PORT', 3001),
+    'PORT': os.environ.get('PORT', 'https://my-fantaze-app.herokuapp.com'),
     'HTTP_STATUS': {
         '404_NOT_FOUND': 404,
         '200_OK': 200,
